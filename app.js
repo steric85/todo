d2ebaa78c00453d90dc234db90400a6b6e6a57e5
steric85 <aneemeh@gmail.com>
2016-05-10 21:57:59 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+(function(){
+
+	var app=angular.module('todolist',[]);
+
+	var tasks=[
+		{
+			taskName : "Bring Snacks",
+			done : true,
+		},
+		{
+			taskName : "Buy Books",
+			done : false,
+		}
+	];
+
+	app.controller('ListController',function(){
+		this.taskList = tasks;
+
+		this.isDone=function(task)
+		{
+			return task.done;
+		};
+
+		this.addTask=function(){
+			
+			this.taskList.push({
+				taskName : this.taskname,
+				done : false,
+			});
+			this.taskname="";
+		}
+	});
+
+
+})();
